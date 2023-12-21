c4677f295d26e0d1df11d871574d2db80c7ab258
Krunoslav Lujic <python.developer.edukacija@gmail.com>
2023-12-21 19:01:23 +0100

    Create py_it_text_02.py

---
+++ py_it_text_02.py
@@ -0,0 +1,23 @@
+# Generated 2 paragraphs, 160 words
+
+
+text = ('Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
+        'Duis convallis lorem efficitur lorem luctus, et aliquam orci accumsan. '
+        'Integer ligula eros, aliquet interdum massa sed, aliquam suscipit lectus. '
+        'Nunc dapibus, nisl vel finibus tempor, turpis purus suscipit ipsum, sed euismod justo magna ac lorem. '
+        'Pellentesque placerat mauris quis nulla interdum, at suscipit ipsum vulputate. '
+        'Proin efficitur nunc dui, vulputate faucibus quam rutrum sed. '
+        'Pellentesque orci diam, vehicula sit amet nulla vitae, iaculis consectetur nulla. '
+        'Phasellus eleifend purus lorem, et gravida nunc aliquam et. '
+        'Morbi neque mauris, vestibulum at lorem luctus, condimentum viverra mauris. '
+        ' '
+        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; '
+        'Vivamus nec placerat lectus, sed pharetra sem. '
+        'Proin feugiat dapibus ante et varius. Sed quis dolor purus. '
+        'Maecenas porttitor est non risus dapibus volutpat. '
+        'Pellentesque dapibus luctus sapien in feugiat. '
+        'Nulla eu consectetur ipsum. '
+        'Sed in vulputate diam. '
+        'Nunc lacinia, dolor nec facilisis suscipit, lacus nibh rhoncus ex, vitae venenatis enim orci eget nunc.')
+
+print(text)
